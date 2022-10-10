c27e8097fdd700fbf51af539a7d55101452442ba
一九五 <2259761058@qq.com>
2022-10-10 11:46:52 +0800

    Organize file locations

---
+++ example/test/test_pir.py
@@ -0,0 +1,30 @@
+from machine import Pin
+import time
+
+pri_pin = Pin(16, Pin.IN, Pin.PULL_DOWN)
+
+start_time = time.ticks_ms()
+
+def main():
+    trigger_flag = 0
+    trigger_time = 0
+    while True:
+        status = pri_pin.value()
+        print("Runtime:" + str(int((time.ticks_ms() - start_time) / 1000)))
+        print("Status:" + str(status))
+        
+        if status is 1:
+            if trigger_flag is 0:
+                trigger_flag = 1
+                trigger_time = time.ticks_ms()
+
+        else:
+            if trigger_flag is 1:
+                trigger_flag = 0
+                trigger_time = int((time.ticks_ms() - trigger_time) / 1000)
+                print("Trigger time:"  + str(trigger_time))
+        
+        time.sleep(1)
+        
+        
+main()
